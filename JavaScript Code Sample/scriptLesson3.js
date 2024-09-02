3940516b2f828e5b60632649cb9081e70c378b32
Ninh Nguyen <95522691+ninhnguyen01@users.noreply.github.com>
2024-09-01 22:46:21 -0700

    Delete JavaScript Code Sample directory

--- JavaScript Code Sample/scriptLesson3.js
+++
@@ -1,7 +0,0 @@
-const character = "Hello";
-const count = 8;
-const rows = [];
-
-for (let i = 0; i < count; i = i + 1) {
-    console.log(i);
-}
--- JavaScript Code Sample/scriptLesson4.js
+++
@@ -1,7 +0,0 @@
-function addTwoNumbers(num1, num2) {
-    return num1 + num2;
-}
-let sum;
-sum = addTwoNumbers(5, 10);
-console.log(sum);
-
