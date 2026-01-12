36f049c7531df29d943a39fb78649349fba7b9fb
Ninh <95522691+ninhnguyen01@users.noreply.github.com>
2026-01-11 21:42:19 -0800

    Delete TS-Practice directory

--- TS-Practice/build/js/assertion/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let alpha = 'hello';
-let beta = alpha;
-let charlie = alpha;
-const year = document.getElementById("year");
-const thisYear = new Date().getFullYear().toString();
-year.setAttribute("datetime", thisYear);
-year.textContent = thisYear;
