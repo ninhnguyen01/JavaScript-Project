e11f8a92693995a84ca90401e6af5c986a1665dd
Ninh <95522691+ninhnguyen01@users.noreply.github.com>
2026-01-11 21:34:30 -0800

    Delete projectReact directory

--- projectReact/react-render/src/UserGreeting.jsx
+++
@@ -1,9 +0,0 @@
-function userGreeting(props) {
-
-    const welcomeMsg = <h2 className="welcome-msg">Welcome {props.username}</h2>;
-    const loginPrompt = <h2 className="login-prompt">Please login to continue</h2>;
-
-    return (props.isLoggedIn ? welcomeMsg : loginPrompt);
-}
-
-export default userGreeting
